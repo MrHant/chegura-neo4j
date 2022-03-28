df5c30b8787048b95e6d3e95aabbfe29533453a5
Artsiomi Silivonchyk <mr.hant@gmail.com>
2022-03-28 10:13:24 +0300

    Refactor getting fen from db

--- chegura-neo4j-core/app/db.py
+++ chegura-neo4j-core/app/db.py
@@ -8,6 +8,20 @@ class DB(object):
     def close(self):
         self.driver.close()
 
+    def _parse_fen_with_relations(self, row):
+        fen = row['fen']
+        best_move = None
+        for move in row['moves']:
+            if best_move is None or move.get('evaluation_value') >= best_move.get('evaluation_value'):
+                best_move = move
+
+        if best_move.get('move') != '':
+            return [fen, {
+                "bestMove": best_move.get('move'),
+                "score": best_move.get('evaluation_value'),
+                "depth": best_move.get('evaluation_depth')
+            }]
+
     def get_fen_with_relations(self, fen):
         with self.driver.session() as session:
             result = session.run("""
@@ -16,18 +30,7 @@ class DB(object):
              RETURN DISTINCT b.fen as fen, collect(r) as moves
              """, fen=fen)
             for row in result:
-                fen = row['fen']
-                best_move = None
-                for move in row['moves']:
-                    if best_move is None or move.get('evaluation_value') >= best_move.get('evaluation_value'):
-                        best_move = move
-
-                if best_move.get('move') != '':
-                    yield [fen, {
-                        "bestMove": best_move.get('move'),
-                        "score": best_move.get('evaluation_value'),
-                        "depth": best_move.get('evaluation_depth')
-                    }]
+                yield self._parse_fen_with_relations(row)
 
     def get_all_fen_with_relations(self):
         with self.driver.session() as session:
@@ -37,16 +40,5 @@ class DB(object):
              RETURN DISTINCT b.fen as fen, collect(r) as moves
              """)
             for row in result:
-                fen = row['fen']
-                best_move = None
-                for move in row['moves']:
-                    if best_move is None or move.get('evaluation_value') >= best_move.get('evaluation_value'):
-                        best_move = move
-
-                if best_move.get('move') != '':
-                    yield [fen, {
-                        "bestMove": best_move.get('move'),
-                        "score": best_move.get('evaluation_value'),
-                        "depth": best_move.get('evaluation_depth')
-                    }]
+                yield self._parse_fen_with_relations(row)
 
